20378c4dff6a1027ee944b57df9dd4c350dfa9ff
Pilation <bogdanleontovich@gmail.com>
2021-11-05 14:57:03 +0200

    1

--- js/script.js
+++ js/script.js
@@ -72,7 +72,80 @@ $('.best-sellers__list').slick({
 
     // ],
 });
-// });
+$('.featured-pages__list').slick({
+    // accessibility: true;
+    arrows: false,
+    dots: false,
+    draggable: true,
+    respondTo: 'window',
+    // адаптивная высота
+    adaptiveHeight: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: 'linear',
+    infinite: true,
+    // or false
+    initialSlide: 0,
+    // autoplay, при infinite - true
+    autoplay: false,
+    autoplaySpeed: 4500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: false,
+    // перетягивание мышкой и телефон
+    draggable: true,
+    swipe: true,
+    touchThreshold: 5,
+    touchMove: true,
+    // дождаться окончания анимации при переключении
+    waitForAnimate: true,
+    centerMode: true,
+    // centerPadding: '-30px',
+    // при true ширина не определяется автоматически, слайды слипаются вместе а те что не влезают, обрезаются сбоку
+    variableWidth: true,
+    // ряды
+    // rows: 1,
+    // slidesPerRow: 1,
+    // 1 слайд как на презентации, листаются по 1 слайду онли
+    fade: false,
+    // переместить стрелки в див
+    // appendArrows: $(".arws"),
+    // переместить стрелки в див
+    // appendDots: $(".arws"),
+    // swipe: true,
+    touchMove: true,
+    // переключение слайдов с клика
+    focusOnSelect: true,
+    // responsive: [
+    //     {
+    //         breakpoint: 1400,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 1,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 1024,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 1,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 800,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1,
+    //             centerMode: true,
+    //             slidePerRow: 1,
+
+    //         }
+    //     }
+
+    // ],
+});
+
 
 
 $(document).ready(function () {
