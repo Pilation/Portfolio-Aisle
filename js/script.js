3db412ad7c0c6fa5649d7906565514720e798cfa
Pilation <bogdanleontovich@gmail.com>
2021-11-11 18:06:29 +0200

    media

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $('.best-sellers__list').slick({
     // дождаться окончания анимации при переключении
     waitForAnimate: true,
     centerMode: true,
-    // centerPadding: '-30px',
+    // centerPadding: '-30px 30px',
     // при true ширина не определяется автоматически, слайды слипаются вместе а те что не влезают, обрезаются сбоку
     variableWidth: true,
     // ряды
@@ -44,33 +44,36 @@ $('.best-sellers__list').slick({
     touchMove: true,
     // переключение слайдов с клика
     focusOnSelect: true,
-    // responsive: [
-    //     {
-    //         breakpoint: 1400,
-    //         settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 1,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 1024,
-    //         settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 1,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 800,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1,
-    //             centerMode: true,
-    //             slidePerRow: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: false,
 
-    //         }
-    //     }
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: false,
 
-    // ],
+            }
+        }
+
+    ],
 });
 $('.featured-pages__list').slick({
     // accessibility: true;
@@ -117,33 +120,36 @@ $('.featured-pages__list').slick({
     touchMove: true,
     // переключение слайдов с клика
     focusOnSelect: true,
-    // responsive: [
-    //     {
-    //         breakpoint: 1400,
-    //         settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 1,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 1024,
-    //         settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 1,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 800,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1,
-    //             centerMode: true,
-    //             slidePerRow: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: false,
 
-    //         }
-    //     }
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: false,
 
-    // ],
+            }
+        }
+
+    ],
 });
 
 
